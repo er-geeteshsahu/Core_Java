ebdd4b413f1a2a9abbdc5f10c0240833533851e4
Geetesh Kumar <sahugeetesh434@gmail.com>
2023-06-04 15:11:55 -0500

    Variables in java

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    public static void main(String args[]){
+
+
+        //        variables in java
+
+        int a = 10;
+        System.out.println(a);
+         a = 5;
+        System.out.println(a);
+
+        String name = "Jhon";
+        System.out.println(name);
+
+
+
+
+
+
+    }
+}
